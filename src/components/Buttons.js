a582f487bb1ccad7ef35654db6ca7374a29a03a5
SwethaParished <swethaparished@gmail.com>
2023-08-10 23:12:02 -0500

    Completed the assignment with the styling added

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,23 @@
-const Buttons = () => {
+import "./styling.css";
+import satData from "./satData";
+
+
+const Buttons = (props) => {
+  let {filterByType, setSat, displaySats } = props;
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {/* <button>Placeholder Button</button> */}
+      {displaySats.map((sat, id) => {
+  return (
+    <button onClick={() => filterByType(sat)} key={id}>
+      {sat} Orbit
+    </button>
+  );
+})}
+      {/* <button>All Orbits</button> */}
+      <button onClick = {() => setSat(satData)}>All Orbits</button>
       </div>
   );
-};
+}
 
 export default Buttons;
